b4553aaf5c3fd211dd79c43c8568189698b3ceb1
Andrew Kim Joseph <andrewkimjoseph@gmail.com>
2023-05-22 15:38:33 +0300

    Committing local changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/andrewkimjoseph/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/andrewkimjoseph/alx-low_level_programming/blob/96b4c237eada16a71ae636e88f7120b5150d013c/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
